b5b68cf965fa3127c4cf7df5ccda0043d69da2d1
Faza Mafatikhul Huda <fazamafatikhulhuda@students.untidar.ac.id>
2026-08-27 21:52:19 +0700

    Add files via upload

---
+++ identitas.py
@@ -0,0 +1,20 @@
+# Menampilkan Data Diri
+print("\nDATA DIRI MAHASISWA")
+
+# Menampilkan Nama
+print("\tNama \t\t\t\t\t: Faza Mafatikhul Huda")
+
+# Menampilkan NIM
+print("\tNIM \t\t\t\t\t: 2605060012")
+
+# Menampilkan Tempat Tanggal Lahir
+print("\tTempat, Tanggal Lahir \t: Magelang, 15 November 2007")
+
+# Menampilkan Alamat
+print("\tAlamat \t\t\t\t\t: Banjarsari Grabag Magelang")
+
+# Menampilkan Jenis Kelamin
+print("\tJenis Kelamin \t\t\t: Laki-laki")
+
+# Menampilkan Hobi
+print("\tHobi \t\t\t\t\t: Menonton Sepak Bola")

c0dc056c4a2d6919e67c20612cd4db1676fd14d5
Faza Mafatikhul Huda <fazamafatikhulhuda@students.untidar.ac.id>
2026-09-03 20:12:32 +0700

    Add files via upload

---
+++ 0012_Faza_Literal_Data.py
@@ -0,0 +1,43 @@
+# Nama   : Faza Mafatikhul Huda
+# NPM    : 2605060012
+# Rombel : 01
+# Tugas  : Literal Data
+
+# Program Pertama
+nama = "Faza Mafatikhul Huda"
+umur = 18
+berat = 42
+
+print("Nama :", nama)
+print("Umur :", umur, "Tahun")
+print("Berat:", berat, "Kg")
+
+# Program Kedua
+angka_string = "123"
+data_1 = int(angka_string)
+print(data_1,type(data_1))
+print(data_1)
+
+angka_float = 45.67
+data_2= int(angka_float)
+print(data_2,type(data_2))
+print(data_2)
+
+angka_integer = 89
+data_3= float(angka_integer)
+print(data_3,type(data_3))
+print(data_3)
+
+angka_integer = 89
+data_4 = str(angka_integer)
+print(data_4,type(data_4))
+print(data_4)
+
+# Program Ketiga
+usia = int(input("Masukkan usia Anda: "))
+tinggi_badan = float(input("Masukkan tinggi badan Anda: "))
+nama = input("Masukkan nama Anda: ")
+
+print("Usia :", usia, "tahun")
+print("Tinggi badan :", tinggi_badan, "cm")
+print("Nama :", nama)
